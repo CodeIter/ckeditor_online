2176e33bc2241db9159254c1b64c519e3bb17e16
Mohamed Amin Boubaker <codeiter-itdev@outlook.com>
2016-11-30 12:19:55 +0100

    added config.js for ckeditor_4.6.0_full
    Signed-off-by: Mohamed Amin Boubaker <codeiter-itdev@outlook.com>

--- config.js
+++ config.js
@@ -4,7 +4,28 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+  // Define changes to default configuration here. For example:
+  // config.language = 'fr';
+  // config.uiColor = '#F7B42C'; // #AADC6E:green ; #F7B42C:orange
+  config.height = 600;
+  config.toolbarCanCollapse = true;
+  
+  config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+  
 };
