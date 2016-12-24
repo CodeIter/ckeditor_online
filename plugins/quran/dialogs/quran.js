280f0a676a772357a9af253fbdebeae503c3c8cb
Mohamed Amin Boubaker <codeiter-itdev@outlook.com>
2016-12-24 13:48:54 +0100

    fix context path to /ckeditor_online in quran plugin
    Signed-off-by: Mohamed Amin Boubaker <codeiter-itdev@outlook.com>

--- plugins/quran/dialogs/quran.js
+++ plugins/quran/dialogs/quran.js
@@ -1,4 +1,4 @@
-CKEDITOR.dialog.add( 'quranDialog', function ( editor ) {
+CKEDITOR.dialog.add('quranDialog', function (editor) {
   return {
     title: 'Quran',
     minWidth: 400,
@@ -8,52 +8,53 @@ CKEDITOR.dialog.add( 'quranDialog', function ( editor ) {
         id: 'tab-basic',
         label: 'Basic Settings',
         elements: [{
-                    // UI elements of the first tab will be defined here.
-                    type: 'text',
-                    id: 'quran',
-                    label: 'Fill the number of Ayat preceded by the number of Sura, ie: 4:3-9 or 1:1-2',
-                    validate: CKEDITOR.dialog.validate.notEmpty( "Abbreviation field cannot be empty." )
-                  }]
+            // UI elements of the first tab will be defined here.
+            type: 'text',
+            id: 'quran',
+            label: 'Fill the number of Ayat preceded by the number of Sura, ie: 4:3-9 or 1:1-2',
+            validate: CKEDITOR.dialog.validate.notEmpty("Abbreviation field cannot be empty.")
+          }]
       },
     ],
-    onOk: function() {
+    onOk: function () {
 
       var dialog = this;
 
 
-      var ayateQueryInput = dialog.getValueOf( 'tab-basic', 'quran' );
+      var ayateQueryInput = dialog.getValueOf('tab-basic', 'quran');
 
       var suraIndex = getSuraIndex(ayateQueryInput);
       var ayatRange = getAyatRange(ayateQueryInput);
 
       $.ajax({
-        url: '/plugins/quran/quran.xml',
+        url: '/ckeditor_online/plugins/quran/quran.xml',
       })
-      .done(function(data){
-        parseQuranData(data, suraIndex, ayatRange);
-      })
-      .fail(function(error) {
-        console.log(error);
-      })
-      .always(function() {
-        console.log("complete");
-      });
+              .done(function (data) {
+                parseQuranData(data, suraIndex, ayatRange);
+              })
+              .fail(function (error) {
+                console.log(error);
+              })
+              .always(function () {
+                console.log("complete");
+              });
 
     }
   };
-  function parseQuranData (quranXml, suraIndex, ayatRange) {
+  function parseQuranData(quranXml, suraIndex, ayatRange) {
 
     var aya, ayat = '';
     var ayatRangeStart = getAyatRangeStart(ayatRange);
     var ayatRangeEnd = getAyatRangeEnd(ayatRange);
 
-    ayat ='<span style="text-align:right; direction:rtl;">';
+    ayat = '<span style="text-align:right; direction:rtl;">';
     for (var i = ayatRangeStart; i <= ayatRangeEnd; i++) {
       aya = getAya(quranXml, suraIndex, i);
-      ayat += renderAya(aya) +' '+ renderAyaNumber(i);
-    };
+      ayat += renderAya(aya) + ' ' + renderAyaNumber(i);
+    }
+    ;
     ayat += '</span>';
-    var ayatElement = editor.document.createElement( 'span' );
+    var ayatElement = editor.document.createElement('span');
     ayatElement.addClass('ayat');
 
     ayatElement.setHtml(ayat);
@@ -61,37 +62,37 @@ CKEDITOR.dialog.add( 'quranDialog', function ( editor ) {
 
   }
   //helpers
-  function getSuraIndex(query){
+  function getSuraIndex(query) {
     var expression = query.split(':');
     return expression[0];
   }
-  function getAyatRange(query){
+  function getAyatRange(query) {
     var expression = query.split(':');
     return expression[1];
   }
-  function getAyatRangeStart(ayatRange){
+  function getAyatRangeStart(ayatRange) {
     console.log(ayatRange);
     var expression = ayatRange.split('-');
     return expression[0];
   }
 
-  function getAyatRangeEnd(ayatRange){
+  function getAyatRangeEnd(ayatRange) {
     var expression = ayatRange.split('-');
     return expression[1];
   }
 
-  function getAya(quranXml, suraIndex, ayaIndex){
-    return $(quranXml).find("sura[index='" + suraIndex  +"']")
-                      .find("aya[index='" + ayaIndex  +"']")
-                      .attr('text');
+  function getAya(quranXml, suraIndex, ayaIndex) {
+    return $(quranXml).find("sura[index='" + suraIndex + "']")
+            .find("aya[index='" + ayaIndex + "']")
+            .attr('text');
   }
 
-  function renderAya(aya){
-    return '<span class="aya">'+ aya +'</span>';
+  function renderAya(aya) {
+    return '<span class="aya">' + aya + '</span>';
   }
 
-  function renderAyaNumber(number){
-    return '<span class="aya-number number-'+ number +'">('+ number +')</span>';
+  function renderAyaNumber(number) {
+    return '<span class="aya-number number-' + number + '">(' + number + ')</span>';
   }
 
 });
